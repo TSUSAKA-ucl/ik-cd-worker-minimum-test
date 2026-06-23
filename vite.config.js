b9059f31d7ca77b5d360cb7ef6995e2a0f31b900
Yuji Tsusaka <tsusaka@ucl.nuee.nagoya-u.ac.jp>
2026-06-23 13:26:17 +0900

    collider付きpiper 2台用

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,17 @@ import { defineConfig } from 'vite'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      onwarn(warning, defaultHandler) {
+        // "use client" の警告だけをスルーする
+        if (warning.code === 'MODULE_LEVEL_DIRECTIVE') {
+          return
+        }
+        defaultHandler(warning)
+      },
+    },
+  },
   plugins: [
     basicSsl()
   ],
